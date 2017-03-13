399fadc1de424174ab1c16f0f49364577ba5398e
Joseph Vu <jvu009@ucr.edu>
2017-03-13 11:16:55 -0700

    add onEnter and beforeExit hooks

--- lib/Route.jsx
+++ lib/Route.jsx
@@ -3,9 +3,22 @@ import { inject, observer } from 'mobx-react';
 
 @inject('store') @observer
 class Route extends Component {
+  componentDidMount() {
+    if (this.props.hooks && this.props.hooks.onEnter) {
+      this.props.hooks.onEnter();
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.hooks && this.props.hooks.beforeExit) {
+      this.props.hooks.beforeExit();
+    }
+  }
+
   render() {
     return (
       <this.props.component
+        params={this.props.store.routes.params}
         children={this.props.children}
         routes={this.props.store.routes}
       />
@@ -15,7 +28,8 @@ class Route extends Component {
 
 Route.propTypes = {
   children: React.PropTypes.any.isRequired,
-  store: React.PropTypes.object.isRequired
+  store: React.PropTypes.object.isRequired,
+  hooks: React.PropTypes.any
 };
 
 export default Route;
