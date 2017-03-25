a4b37da08779a6ed32dbc8f1a9340a4e1803a984
Brandon Smith <bxleesmith@gmail.com>
2017-03-24 23:49:49 -0700

    adding router and index

---
+++ lib/vanilla/Router.jsx
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
+
+function parsedRoute(routePath, currPath) {
+  if (!routePath) return;
+  let newPath = { routePath, params: {} };
+  if (routePath.includes(':')) {
+    const paramsObj = {};
+    const routePaths = routePath.match(/[:a-zA-Z0-9_]+/g);
+    const urlPath = currPath.match(/[:a-zA-Z0-9_]+/g);
+    let modifiedUrl = '';
+
+    if (routePaths && urlPath && routePaths.length === urlPath.length) {
+      for (let i = 0; i < urlPath.length; i += 1) {
+        if (!(routePaths[i] === urlPath[i]) && routePaths[i].slice(0, 1) === ':') {
+          paramsObj[routePaths[i].slice(1)] = urlPath[i];
+          modifiedUrl = `${modifiedUrl}/${urlPath[i]}`;
+        } else {
+          modifiedUrl = `${modifiedUrl}/${routePaths[i]}`;
+        }
+      }
+    }
+
+    newPath.params = paramsObj;
+    newPath.routePath = modifiedUrl;
+  }
+
+  return newPath;
+}
+
+function returnArray(objectOrArray) {
+  return Array.isArray(objectOrArray) ? objectOrArray : [objectOrArray];
+}
+
+function getIndexRoutes(currRoute) {
+  let indexRoute = [];
+  if (currRoute.props.children) {
+    indexRoute = returnArray(currRoute.props.children).filter(route => route.props.index);
+  }
+
+  return indexRoute;
+}
+
+function getQueries() {
+  if (location.search) {
+    return location.search.split(/&amp;/gi)
+      .reduce((queries, cv) => {
+        cv = cv.replace(/%20/gi, ' ').split('=');
+        const key = cv[0].replace(/^\?/, '');
+        const value = cv[1];
+        queries[key] = value;
+        return queries;
+      }, {});
+  }
+}
+
+function findComponent(pathToFind, routes, updatePath) {
+  // check if valid input
+  if (!pathToFind) throw new Error('Must enter valid path.');
+  if (!Array.isArray(routes)) throw new Error('Input must be an array.');
+
+  let parent = null;
+  let children = null;
+  let params = {};
+  let queries = {};
+
+  for (let i = 0; i < routes.length; i++) {
+    if (!routes[i]) break;
+
+    let check = parsedRoute(routes[i].props.path, pathToFind);
+    if (check && check.routePath === pathToFind) {
+      children = getIndexRoutes(routes[i]);
+      params = check.params;
+      parent = routes[i];
+      break;
+    }
+
+    if (routes[i].props.children) {
+      if (!Array.isArray(routes[i].props.children)) {
+        children = findComponent(pathToFind, [routes[i].props.children]);
+      } else {
+        children = findComponent(pathToFind, routes[i].props.children);
+      }
+    }
+
+    if (children) {
+      parent = routes[i];
+      break;
+    }
+  }
+  // check for queries before render
+  queries = getQueries();
+
+  return parent ? React.cloneElement(parent, { children, params, queries, updatePath }) : null;
+}
+
+class Router extends Component {
+
+  updatePath(e) {
+    e.preventDefault();
+    history.push(e.target.getAttribute('href'));
+  }
+
+  render() {
+    const currPath = history.location.pathname;
+
+    let componentToRender = findComponent(currPath, returnArray(this.props.children), this.updatePath);
+
+    if (!componentToRender) {
+      const lastChild = this.props.children[this.props.children.length - 1];
+      if (lastChild.props.path === '*') {
+        componentToRender = lastChild;
+      }
+    }
+
+    return (
+      <div>
+        {componentToRender}
+      </div>
+    );
+  }
+}
+
+Router.propTypes = {
+  children: React.PropTypes.any
+};
+
+export default Router;
